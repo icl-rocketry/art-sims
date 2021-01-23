749e61f0b973fd2c21dd95cab5b10f926506115d
Ishaan Alidina <ishaan.alidina19@imperial.ac.uk>
2021-01-23 12:09:37 +0000

    Adding files

---
+++ FinLETemp.m
@@ -0,0 +1,10 @@
+%Script to calcalate the temperatures of fin LEs at a specified velocity and altitude
+%Based off wiki article by Devansh Agarwal (2020), and report by Tauber and Menees (1987)
+%Copyright Ishaan Alidina (2021)
+
+%Housekeeping
+clear
+clc
+
+%Initialisation of variables
+[q_dot,~] = NoseconeTipTemp
