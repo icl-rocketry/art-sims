8511dd0aa7682cbcc402373a60fcf96c55f06c5e
NathanaelJ <nathanaelaaronjenkins@gmail.com>
2021-04-14 10:01:16 +0100

    Heating Fortran

--- Heating/TipHeating.F90
+++ Heating/TipHeating.F90
@@ -6,13 +6,13 @@ program TipHeating
     !Nathanael Jenkins (2021)
     !-----------------------------------------------
 
+
     implicit none
     !USER DEFINED VARIABLES
     !Ma => instantaneous mach number; V => instantaneous velocity; rn => body nose radius; rho => atmospheric density; e => emissivity
     double precision :: Ma=0, V=379.66, rn=46.0, rho=1.1655, e=0.92
     !delta => leading edge sweep; X => nose tip-fins length; phi => local body angle (small impact)
     double precision :: delta=30.252, X=0.777, phi1=0, phi2 = 89.99
-
     !OTHER VARIABLES
     !Tw=> final temperature; to=> max' stagnation temperature
     !N, Z => useufl constants (see below); C1, C2 => variable constants; i => iteration counter
@@ -22,6 +22,7 @@ program TipHeating
     !Date and time variables for diaplay
     character(24) :: date
     integer :: i=0
+
 
     !USER VARIABLE DEFINITION
     print '(/, a46)', 'If using pre-programmed conditions, enter Ma=0'
@@ -47,11 +48,12 @@ program TipHeating
     else
         Ma = 1.1232
     end if
-
     !Calculates maximum stagnation temperature
     to=((1+(0.2*(Ma**2)))*288.15)
     
-    ! ------------ NOSE CALCULATIONS --------------
+
+
+    ! ------------ NOSE CALCULATIONS || C-BASED --------------
     !Calculate useful constants N and Z
     !These will be used in every iteration, but only need calculating once, reducing computation time
     N = (((rn/1000)**(-0.5))*((1.83)*10.0**(-8)))
@@ -68,11 +70,11 @@ program TipHeating
         !Increase iteration count by 1
         i = i+1
     end do
-
     !Calculate final temperature
     Tw = ((Z*C1)**0.25)
 
-    ! ------------ FIN CALCULATIONS --------------
+
+    ! ------------ FIN CALCULATIONS || C-BASED --------------
     !Calculate min estimated leading edge heating
     qo = C1*(rho**0.5)*(V**3)
     !Calculates constant C (explicitly)
@@ -83,12 +85,12 @@ program TipHeating
     qle = sqrt((0.5*(qo**2)*((cosd(delta))**2))+((qfp**2)*((sind(delta))**2)))
     !Calculates temperature based on heat transfers
     Tle1 = (qle/(e*((5.67)*(10.0**(-12)))))**0.25
-
     !Calculate max estimated leading edge heating (repeats above process)
     C1 = 2.53*(10.0**(-9))*((cosd(phi2))**(0.5))*(sind(phi2))*(X**(-0.5))*(1-(Tw/to))
     qfp = C1*(rho**(0.5))*(V**(3.2))
     qle = sqrt((0.5*(qo**2)*((cosd(delta))**2))+((qfp**2)*((sind(delta))**2)))
     Tle2 = (qle/(e*((5.67)*(10.0**(-12)))))**0.25
+
 
     ! ------------ DATA OUTPUT --------------
     call fdate(date)
@@ -123,5 +125,49 @@ program TipHeating
     end do
     print *, Tw1
     print *, (Tw1-273.15)
+
+
+    ! ------------ NOSE CALCULATIONS || DIRECT --------------
+    !Calculate useful constants N and Z
+    !These will be used in every iteration, but only need calculating once, reducing computation time
+    N = (((rn/1000)**(-0.5))*((1.83)*10.0**(-8)))
+    Z = (((rho**0.5)*(V**3))/((e)*((5.67)*10.0**(-12))))
+    ! Initialises variables
+    Tw1=to
+    tol = 1.0
+    !Iterate until change in temperature for each iteration is less than 10^-12
+    do while (tol > 10.0**(-12))
+        !Computes a new value for constant C
+        Tw2 = to*(1-(Tw1**4)/(N*Z))
+        !Calculates tolerance based on change in estimated temperature over each iteration
+        tol = abs(Tw2-Tw1)
+        !Re-assigns calculated value of C to 'old' value
+        Tw1=Tw2
+        !Increase iteration count by 1
+        i = i+1
+    end do
+    Tw = Tw1
+
+
+    ! ------------ FIN CALCULATIONS || DIRECT? --------------
+    ! UNDER REVIEW
+
+
+    ! ------------ DATA OUTPUT --------------
+    call fdate(date)
+    print '(/, a54)', '  --ROCKET HEATING CALCULATOR || DIRECT METHOD--      '
+    print '(a51)', '----------------------------------------------------------------'
+    print '(a3, f3.1, a6, f5.1, a6, f4.1, a8, f5.3, a6, f5.2)', 'M: ', Ma, ' | V: ', V, ' | R: ', rn, ' | rho: ', rho, ' | e: ', e
+    print '(a51)', '----------------------------------------------------------------'
+    print '(a32, i3)', '                   Iterations: ', i
+    print '(a33, e11.5, a2)', '                     Tolerance:  ±', tol, ' k'
+    !rint '(a32, f7.3, a2)', 'Maximum stagnation temperature: ', to, ' k'
+    print '(a32, f7.3, a4)', 'Maximum stagnation temperature: ', (to-273.15), ' ºc'
+    !print '(a32, f7.3, a2)', '             Nose  Temperature: ', Tw, ' k'
+    print '(a32, f7.3, a4)', '             Nose Temperature: ', (Tw-273.15), ' ºc'
+    !print '(a32, f7.3, a2)', '          Min Fin  Temperature: ', Tle1, ' k'
+    print '(a32, f7.3, a4)', '          Min Fin Temperature: ', (Tle1-273.15), ' ºc'
+    !print '(a32, f7.3, a2)', '          Max Fin  Temperature: ', Tle2, ' k'
+    print '(a32, f7.3, a4)', '          Max Fin Temperature: ', (Tle2-273.15), ' ºc'
 
 end program Tipheating
